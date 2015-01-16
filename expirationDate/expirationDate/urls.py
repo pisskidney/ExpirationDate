19d8341d692f21a35f31d03e6a8b398c39ff8ab1
Peter Horvath <horvthpeter@gmail.com>
2015-01-16 04:28:37 +0000

    Added auth related url routes

--- expirationDate/expirationDate/urls.py
+++ expirationDate/expirationDate/urls.py
@@ -3,13 +3,15 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.contrib import admin
 
-from expirationDate.views import HomePage
+from expirationDate.views import HomePage, LoginView, LogoutView
 
 urlpatterns = patterns(
     '',
     url(r'^$', HomePage.as_view(), name='home'),
     (r'^grappelli/', include('grappelli.urls')),  # grappelli URLS
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', views.LoginView.as_view(), name='login'),
+    url(r'^logout/', views.LogoutView.as_view(), name='logout'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
--- expirationDate/expirationDate/views.py
+++ expirationDate/expirationDate/views.py
@@ -1,5 +1,17 @@
+from django.views.generic.base import View
 from django.views.generic.base import TemplateView
+from django.shortcuts import redirect, render
 
 
 class HomePage(TemplateView):
     template_name = "homepage.html"
+
+
+class LoginView(View):
+    pass
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('home')
